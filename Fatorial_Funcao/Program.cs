ecbcd3315506efd1d0462d4ac5ad41567e52ec6a
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 10:27:56 -0300

    Adicionar arquivos de projeto.

---
+++ Fatorial_Funcao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fatorial_Funcao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            int num = int.Parse(Console.ReadLine());
+            int resp = fat(num);
+            Console.WriteLine($"fatorial de {num} é {resp}");
+        }
+
+        static int fat (int num)
+        {
+            if (num == 0)
+                return 1;
+            else
+                return (num * (num - 1));
+        }
+    }
+}
